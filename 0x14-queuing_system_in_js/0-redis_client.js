b37d02259bfdb134239a4f719bf5b7d0fcc7f956
luiscarvil <luiscarvajal05@gmail.com>
2022-04-19 19:11:23 -0500

    Add redis client first task

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+'use strict';
+import {createClient} from 'redis';
+const client = createClient();
+
+client.on("error", (error) => {
+  if (error) console.log(`Redis client not connected to the server: ${error}`)
+}).on('ready', () => {
+    console.log('Redis client connected to the server');
+});
